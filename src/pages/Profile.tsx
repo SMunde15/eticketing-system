391523e43dd733cca835cd0a4ef24b15e14f1bed
Sourabh Munde <sourabh.munde15@gmail.com>
2024-07-20 20:23:26 +0530

    styles

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -14,8 +14,10 @@ import {
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import TopNavBar from '../components/TopNavBar';
+import { useAuth } from '../contexts/AuthContext'; // Assuming you have an AuthContext
 
 const Profile: React.FC = () => {
+  const { userRole } = useAuth();
   const [userData, setUserData] = useState<any>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [isEditing, setIsEditing] = useState(false);
@@ -35,15 +37,21 @@ const Profile: React.FC = () => {
     try {
       const token = Cookies.get('token');
       const response = await axios.get('https://e-ticketing.nexpictora.com/users/details', {
-        withCredentials: true
+        withCredentials: true,
       });
-      setUserData(response.data);
-      setFormData({
-        name: response.data.name || '',
-        email: response.data.email || '',
-        mobileNumber: response.data.mobile || '',
-        age: response.data.age || '',
-      });
+
+      const user = response.data.find((user: any) => user.role === userRole);
+
+      if (user) {
+        setUserData(user);
+        setFormData({
+          name: user.name || '',
+          email: user.email || '',
+          mobileNumber: user.mobile || '',
+          age: user.age || '',
+        });
+      }
+
       setIsLoading(false);
     } catch (error) {
       console.error('Error fetching user data:', error);
@@ -62,7 +70,7 @@ const Profile: React.FC = () => {
     try {
       const token = Cookies.get('token');
       await axios.put('https://e-ticketing.nexpictora.com/users/details', formData, {
-        withCredentials: true
+        withCredentials: true,
       });
       setIsEditing(false);
       fetchUserData();
@@ -81,74 +89,79 @@ const Profile: React.FC = () => {
   }
 
   return (
-    <><TopNavBar />
-    <Container>
-      <Box sx={{ mt: 4 }}>
-        <Typography variant="h4" gutterBottom>
-          Profile
-        </Typography>
-        <Card>
-          <CardContent>
-            {isEditing ? (
-              <>
-                <TextField
-                  label="Name"
-                  name="name"
-                  value={formData.name}
-                  onChange={handleInputChange}
-                  fullWidth
-                  margin="normal" />
-                <TextField
-                  label="Email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleInputChange}
-                  fullWidth
-                  margin="normal"
-                  disabled // Email should typically not be editable
-                />
-                <TextField
-                  label="Mobile Number"
-                  name="mobileNumber"
-                  value={formData.mobileNumber}
-                  onChange={handleInputChange}
-                  fullWidth
-                  margin="normal" />
-                <TextField
-                  label="Age "
-                  name="dateOfBirth"
-                  value={formData.age}
-                  onChange={handleInputChange}
-                  fullWidth
-                  margin="normal"
-                   />
-                <Button variant="contained" color="primary" onClick={handleSave} sx={{ mt: 2 }}>
-                  Save
-                </Button>
-              </>
-            ) : (
-              <>
-                <Typography variant="h6">Name: {userData.name || 'N/A'}</Typography>
-                <Typography variant="h6">Email: {userData.email || 'N/A'}</Typography>
-                <Typography variant="h6">Mobile Number: {userData.mobile || 'N/A'}</Typography>
-                <Typography variant="h6">Date of Birth: {userData.age || 'N/A'}</Typography>
-                <Button variant="contained" color="primary" onClick={() => setIsEditing(true)} sx={{ mt: 2 }}>
-                  Edit
-                </Button>
-              </>
-            )}
-          </CardContent>
-        </Card>
-      </Box>
-      <Snackbar
-        open={showSnackbar}
-        autoHideDuration={6000}
-        onClose={handleSnackbarClose}
-        message="DETAILS UPDATED"
-        ContentProps={{
-          sx: { backgroundColor: '#4caf50' },
-        }} />
-    </Container></>
+    <>
+      <TopNavBar />
+      <Container>
+        <Box sx={{ mt: 4 }}>
+          <Typography variant="h4" gutterBottom>
+            Profile
+          </Typography>
+          <Card>
+            <CardContent>
+              {isEditing ? (
+                <>
+                  <TextField
+                    label="Name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleInputChange}
+                    fullWidth
+                    margin="normal"
+                  />
+                  <TextField
+                    label="Email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    fullWidth
+                    margin="normal"
+                    disabled // Email should typically not be editable
+                  />
+                  <TextField
+                    label="Mobile Number"
+                    name="mobileNumber"
+                    value={formData.mobileNumber}
+                    onChange={handleInputChange}
+                    fullWidth
+                    margin="normal"
+                  />
+                  <TextField
+                    label="Age"
+                    name="age"
+                    value={formData.age}
+                    onChange={handleInputChange}
+                    fullWidth
+                    margin="normal"
+                  />
+                  <Button variant="contained" color="primary" onClick={handleSave} sx={{ mt: 2 }}>
+                    Save
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Typography variant="h6">Name: {userData.name || 'N/A'}</Typography>
+                  <Typography variant="h6">Email: {userData.email || 'N/A'}</Typography>
+                  <Typography variant="h6">Mobile Number: {userData.mobile || 'N/A'}</Typography>
+                  <Typography variant="h6">Age: {userData.age || 'N/A'}</Typography>
+                  <Button variant="contained" color="primary" onClick={() => setIsEditing(true)} sx={{ mt: 2 }}>
+                    Edit
+                  </Button>
+                </>
+              )}
+            </CardContent>
+          </Card>
+        </Box>
+        <Snackbar
+          open={showSnackbar}
+          autoHideDuration={6000}
+          onClose={handleSnackbarClose}
+          message="Details updated"
+          ContentProps={{
+            sx: { backgroundColor: '#4caf50' },
+          }}
+        />
+      </Container>
+    </>
   );
 };
 
